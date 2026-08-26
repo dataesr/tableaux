4e06d6bbedc79e1ce3ce69e0877b0a762e808775
jerem <jeremy.peglion@gmail.com>
2026-08-26 08:36:30 +0200

    fix(ep/evolution-pcri): visualisation des données par défaut #2

--- client/src/boards/european-projects/pages/evolution-pcri/charts/funding-stacked-area/index.tsx
+++ client/src/boards/european-projects/pages/evolution-pcri/charts/funding-stacked-area/index.tsx
@@ -3,7 +3,7 @@ import { useSearchParams } from "react-router-dom";
 
 import { getData } from "./query.js";
 import options from "./options.js";
-import { useGetParams, readingKey, renderDataTable } from "./utils.js";
+import { useGetParams, readingKey } from "./utils.js";
 import ChartWrapper from "../../../../../../components/chart-wrapper";
 import DefaultSkeleton from "../../../../../../components/charts-skeletons/default";
 import { useChartColor } from "../../../../../../hooks/useChartColor";
