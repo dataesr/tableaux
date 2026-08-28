fe4b87e3520e8a55efb7ecb3ea396e0427ad5939
jerem <jeremy.peglion@gmail.com>
2026-08-28 11:33:56 +0200

    feat(ep/admin): ajout du menu sticky pour la page d'audit

--- client/src/boards/admin/accessibility.tsx
+++ client/src/boards/admin/accessibility.tsx
@@ -143,129 +143,135 @@ export default function AccessibilityPage() {
   const changesDetected = formResponses.some((response) => response.modified);
 
   return (
-    <Container className="fr-py-5w" role="main">
-      <Row className="">
-        <Col>
-          <Title as="h2">Audit d'accessibilité pour {dashboardId}</Title>
-          <Text>Date du dernier audit : 01/01/2024</Text>
-          <Text>Version du RGAA testée : {rgaa?.rgaaId} </Text>
-        </Col>
-        <Col md={3}>
-          <fieldset className="fr-m-0">
-            <legend className="fr-mb-2w">Stats</legend>
-            <div>...</div>
-          </fieldset>
-          <Button
-            className="fr-mt-1w"
-            variant="secondary"
-            disabled={!changesDetected}
-            onClick={() => {
-              updateTestsInDb();
-            }}
-          >
-            Enregistrer les modifications
-          </Button>
-        </Col>
-        <Col md={2}>
-          <fieldset>
-            <legend className="fr-mb-2w">Statut des tests</legend>
-            <div className="fr-flex fr-flex-wrap fr-gap-2w">
-              <div className="fr-flex fr-items-center fr-gap-1w">
-                <span className="fr-mr-1w">🟢</span>
-                <span>Réussi</span>
-              </div>
-              <div className="fr-flex fr-items-center fr-gap-1w">
-                <span className="fr-mr-1w">🔴</span>
-                <span>Échoué</span>
-              </div>
-              <div className="fr-flex fr-items-center fr-gap-1w">
-                <span className="fr-mr-1w">🔵</span>
-                <span>Non applicable</span>
-              </div>
-              <div className="fr-flex fr-items-center fr-gap-1w">
-                <span className="fr-mr-1w">⚪</span>
-                <span>Non testé</span>
-              </div>
-            </div>
-          </fieldset>
-        </Col>
-      </Row>
-      <Row className="fr-mt-3w">
-        <section className="fr-accordion">
-          {rgaaFlat.raw?.criteria?.map(({ thematique_id, thematique, criteres }) => (
-            <div key={thematique_id}>
-              <Title as="h3" className="fr-accordion__title">
-                <button type="button" className="fr-accordion__btn" aria-expanded="false" aria-controls={`accordion-${thematique_id}`}>
-                  {thematique}
-                  <Badge className="fr-ml-2w">
-                    {
-                      // nombre de tests réussis 🟢, nombre de tests échoués 🔴, nombre de tests non-applcables 🔵, non-testés ⚪ pour cette thématique
-                      (() => {
-                        const totalTestsThematique = rgaa_tests_list?.filter((t) => t.thematiqueId === thematique_id).length || 0;
-                        const testsOk = audit?.tests?.filter((test) => test.value === "ok" && rgaa_tests_list?.find((t) => t.testId === test.testId)?.thematiqueId === thematique_id).length || 0;
-                        const testsFail = audit?.tests?.filter((test) => test.value === "fail" && rgaa_tests_list?.find((t) => t.testId === test.testId)?.thematiqueId === thematique_id).length || 0;
-                        const testsNa = audit?.tests?.filter((test) => test.value === "na" && rgaa_tests_list?.find((t) => t.testId === test.testId)?.thematiqueId === thematique_id).length || 0;
-                        const totalTestsAuditThematique = audit?.tests?.filter((test) => rgaa_tests_list?.find((t) => t.testId === test.testId)?.thematiqueId === thematique_id).length || 0;
-                        const testsNonTestes =
-                          totalTestsThematique - totalTestsAuditThematique + audit?.tests?.filter((test) => test.value === "initial" && rgaa_tests_list?.find((t) => t.testId === test.testId)?.thematiqueId === thematique_id).length || 0;
-                        return `${testsOk} 🟢 / ${testsFail} 🔴 / ${testsNa} 🔵 / ${testsNonTestes} ⚪`;
-                      })()
-                    }
-                  </Badge>
-                </button>
-              </Title>
-              <div id={`accordion-${thematique_id}`} className="fr-collapse">
-                {criteres?.map(({ id: critereId, titre, tests }) => (
-                  <div key={critereId} className="fr-mt-2w">
-                    <Title as="h4" look="h6">
-                      {titre}
-                    </Title>
-                    {tests?.map(({ id: testId, description }) => (
-                      <div key={testId}>
-                        <Text className="fr-mb-0 fr-mt-4w">
-                          {audit?.tests?.find((test) => test.testId === testId)?.value === "ok"
-                            ? "🟢 "
-                            : audit?.tests?.find((test) => test.testId === testId)?.value === "fail"
-                              ? "🔴 "
-                              : audit?.tests?.find((test) => test.testId === testId)?.value === "na"
-                                ? "🔵 "
-                                : "⚪ "}
-                          {description}
-                        </Text>
-
-                        <Container fluid key={`yiyji${testId}`}>
-                          <Row gutters>
-                            <Col md={2}>
-                              <select className="fr-select" value={getValueFromState(testId, "value")} onChange={(e) => setValueInState(testId, "value", e.target.value)}>
-                                <option value="initial">Non testé</option>
-                                <option value="ok">Réussi</option>
-                                <option value="fail">Échoué</option>
-                                <option value="na">Non applicable</option>
-                              </select>
-                            </Col>
-                            <Col md={7}>
-                              <input type="text" className="fr-input" value={getValueFromState(testId, "comment")} onChange={(e) => setValueInState(testId, "comment", e.target.value)} />
-                            </Col>
-                            <Col>
-                              {audit?.tests?.find((t) => t.testId === testId)?.date
-                                ? new Date(audit.tests.find((t) => t.testId === testId).date).toLocaleDateString("fr-FR", {
-                                    year: "numeric",
-                                    month: "long",
-                                    day: "numeric",
-                                  })
-                                : ""}
-                            </Col>
-                          </Row>
-                        </Container>
-                      </div>
-                    ))}
+    <>
+      <div className="menu-sticky">
+        <Container>
+          <Row>
+            <Col>
+              <Title as="h2">Audit d'accessibilité pour {dashboardId}</Title>
+              <Text>Date du dernier audit : 01/01/2024</Text>
+              <Text>Version du RGAA testée : {rgaa?.rgaaId} </Text>
+            </Col>
+            <Col md={3}>
+              <fieldset className="fr-m-0">
+                <legend className="fr-mb-2w">Stats</legend>
+                <div>...</div>
+              </fieldset>
+              <Button
+                className="fr-mt-1w"
+                variant="secondary"
+                disabled={!changesDetected}
+                onClick={() => {
+                  updateTestsInDb();
+                }}
+              >
+                Enregistrer les modifications
+              </Button>
+            </Col>
+            <Col md={2}>
+              <fieldset>
+                <legend className="fr-mb-2w">Statut des tests</legend>
+                <div className="fr-flex fr-flex-wrap fr-gap-2w">
+                  <div className="fr-flex fr-items-center fr-gap-1w">
+                    <span className="fr-mr-1w">🟢</span>
+                    <span>Réussi</span>
                   </div>
-                ))}
+                  <div className="fr-flex fr-items-center fr-gap-1w">
+                    <span className="fr-mr-1w">🔴</span>
+                    <span>Échoué</span>
+                  </div>
+                  <div className="fr-flex fr-items-center fr-gap-1w">
+                    <span className="fr-mr-1w">🔵</span>
+                    <span>Non applicable</span>
+                  </div>
+                  <div className="fr-flex fr-items-center fr-gap-1w">
+                    <span className="fr-mr-1w">⚪</span>
+                    <span>Non testé</span>
+                  </div>
+                </div>
+              </fieldset>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <Container className="fr-py-5w" role="main">
+        <Row className="fr-mt-3w">
+          <section className="fr-accordion">
+            {rgaaFlat.raw?.criteria?.map(({ thematique_id, thematique, criteres }) => (
+              <div key={thematique_id}>
+                <Title as="h3" className="fr-accordion__title">
+                  <button type="button" className="fr-accordion__btn" aria-expanded="false" aria-controls={`accordion-${thematique_id}`}>
+                    {thematique}
+                    <Badge className="fr-ml-2w">
+                      {
+                        // nombre de tests réussis 🟢, nombre de tests échoués 🔴, nombre de tests non-applcables 🔵, non-testés ⚪ pour cette thématique
+                        (() => {
+                          const totalTestsThematique = rgaa_tests_list?.filter((t) => t.thematiqueId === thematique_id).length || 0;
+                          const testsOk = audit?.tests?.filter((test) => test.value === "ok" && rgaa_tests_list?.find((t) => t.testId === test.testId)?.thematiqueId === thematique_id).length || 0;
+                          const testsFail = audit?.tests?.filter((test) => test.value === "fail" && rgaa_tests_list?.find((t) => t.testId === test.testId)?.thematiqueId === thematique_id).length || 0;
+                          const testsNa = audit?.tests?.filter((test) => test.value === "na" && rgaa_tests_list?.find((t) => t.testId === test.testId)?.thematiqueId === thematique_id).length || 0;
+                          const totalTestsAuditThematique = audit?.tests?.filter((test) => rgaa_tests_list?.find((t) => t.testId === test.testId)?.thematiqueId === thematique_id).length || 0;
+                          const testsNonTestes =
+                            totalTestsThematique - totalTestsAuditThematique + audit?.tests?.filter((test) => test.value === "initial" && rgaa_tests_list?.find((t) => t.testId === test.testId)?.thematiqueId === thematique_id).length || 0;
+                          return `${testsOk} 🟢 / ${testsFail} 🔴 / ${testsNa} 🔵 / ${testsNonTestes} ⚪`;
+                        })()
+                      }
+                    </Badge>
+                  </button>
+                </Title>
+                <div id={`accordion-${thematique_id}`} className="fr-collapse">
+                  {criteres?.map(({ id: critereId, titre, tests }) => (
+                    <div key={critereId} className="fr-mt-2w">
+                      <Title as="h4" look="h6">
+                        {titre}
+                      </Title>
+                      {tests?.map(({ id: testId, description }) => (
+                        <div key={testId}>
+                          <Text className="fr-mb-0 fr-mt-4w">
+                            {audit?.tests?.find((test) => test.testId === testId)?.value === "ok"
+                              ? "🟢 "
+                              : audit?.tests?.find((test) => test.testId === testId)?.value === "fail"
+                                ? "🔴 "
+                                : audit?.tests?.find((test) => test.testId === testId)?.value === "na"
+                                  ? "🔵 "
+                                  : "⚪ "}
+                            {description}
+                          </Text>
+
+                          <Container fluid key={`yiyji${testId}`}>
+                            <Row gutters>
+                              <Col md={2}>
+                                <select className="fr-select" value={getValueFromState(testId, "value")} onChange={(e) => setValueInState(testId, "value", e.target.value)}>
+                                  <option value="initial">Non testé</option>
+                                  <option value="ok">Réussi</option>
+                                  <option value="fail">Échoué</option>
+                                  <option value="na">Non applicable</option>
+                                </select>
+                              </Col>
+                              <Col md={7}>
+                                <input type="text" className="fr-input" value={getValueFromState(testId, "comment")} onChange={(e) => setValueInState(testId, "comment", e.target.value)} />
+                              </Col>
+                              <Col>
+                                {audit?.tests?.find((t) => t.testId === testId)?.date
+                                  ? new Date(audit.tests.find((t) => t.testId === testId).date).toLocaleDateString("fr-FR", {
+                                      year: "numeric",
+                                      month: "long",
+                                      day: "numeric",
+                                    })
+                                  : ""}
+                              </Col>
+                            </Row>
+                          </Container>
+                        </div>
+                      ))}
+                    </div>
+                  ))}
+                </div>
               </div>
-            </div>
-          ))}
-        </section>
-      </Row>
-    </Container>
+            ))}
+          </section>
+        </Row>
+      </Container>
+    </>
   );
 }
