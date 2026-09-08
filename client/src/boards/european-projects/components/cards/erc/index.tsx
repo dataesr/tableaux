39a052d2d8d3722530ab37450ccb3b9e86cb8524
jerem <jeremy.peglion@gmail.com>
2026-09-08 16:13:26 +0200

    fix(ep/home): maj des couleurs MSCA et ERC

--- client/src/boards/european-projects/components/cards/erc/index.tsx
+++ client/src/boards/european-projects/components/cards/erc/index.tsx
@@ -1,31 +1,23 @@
-import { Text } from "@dataesr/dsfr-plus";
+import { Badge } from "@dataesr/dsfr-plus";
 import { Link } from "react-router-dom";
 
 export { default as ErcSynthesisCards } from "./synthesis-cards";
 export { default as ErcDestinationCards } from "./destination-cards";
 export { default as ErcPanelCards } from "./panel-cards";
 
-export default function ErcCard({
-  title,
-  subtitle,
-  description,
-  to,
-  titleAs: Heading = "h3",
-}: {
-  title: string;
-  subtitle?: string;
-  description?: string;
-  to: string;
-  titleAs?: "h2" | "h3" | "h4" | "h5";
-}) {
+export default function ErcCard({ title, subtitle, description, to, titleAs: Heading = "h3" }: { title: string; subtitle?: string; description?: string; to: string; titleAs?: "h2" | "h3" | "h4" | "h5" }) {
   return (
-    <div className="fr-card fr-enlarge-link" style={{ borderBottom: "4px solid #03a037ff" }}>
+    <div className="fr-card fr-enlarge-link" style={{ borderBottom: "4px solid var(--text-label-orange-terre-battue)" }}>
       <div className="fr-card__body">
         <div className="fr-card__content">
           <Heading className="fr-card__title">
             <Link to={to}>{title}</Link>
           </Heading>
-          {subtitle && <Text>{subtitle}</Text>}
+          {subtitle && (
+            <Badge className="fr-mb-2w" color="orange-terre-battue">
+              {subtitle}
+            </Badge>
+          )}
           {description && <p className="fr-card__desc">{description}</p>}
         </div>
       </div>
--- client/src/boards/european-projects/components/cards/msca/index.tsx
+++ client/src/boards/european-projects/components/cards/msca/index.tsx
@@ -1,31 +1,23 @@
-import { Text } from "@dataesr/dsfr-plus";
+import { Badge } from "@dataesr/dsfr-plus";
 import { Link } from "react-router-dom";
 
 export { default as MscaSynthesisCards } from "./synthesis-cards";
 export { default as MscaDestinationCards } from "./destination-cards";
 export { default as MscaPanelCards } from "./panel-cards";
 
-export default function MscaCard({
-  title,
-  subtitle,
-  description,
-  to,
-  titleAs: Heading = "h3",
-}: {
-  title: string;
-  subtitle?: string;
-  description?: string;
-  to: string;
-  titleAs?: "h2" | "h3" | "h4" | "h5";
-}) {
+export default function MscaCard({ title, subtitle, description, to, titleAs: Heading = "h3" }: { title: string; subtitle?: string; description?: string; to: string; titleAs?: "h2" | "h3" | "h4" | "h5" }) {
   return (
-    <div className="fr-card fr-enlarge-link" style={{ borderBottom: "4px solid #03a037ff" }}>
+    <div className="fr-card fr-enlarge-link" style={{ borderBottom: "4px solid var(--text-label-green-menthe)" }}>
       <div className="fr-card__body">
         <div className="fr-card__content">
           <Heading className="fr-card__title">
             <Link to={to}>{title}</Link>
           </Heading>
-          {subtitle && <Text>{subtitle}</Text>}
+          {subtitle && (
+            <Badge className="fr-mb-2w" color="green-menthe">
+              {subtitle}
+            </Badge>
+          )}
           {description && <p className="fr-card__desc">{description}</p>}
         </div>
       </div>
